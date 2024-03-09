8a5deeddc52fc8b94f2fea2a727e2d3665e95c3a
Anden Acitelli <a.acitelli@gmail.com>
2024-03-09 17:38:24 -0600

    fix/update some referral links

--- src/data/referral-links.ts
+++ src/data/referral-links.ts
@@ -4,20 +4,16 @@ const getRandom = (array: string[]): string => {
 
 export const REFERRALS = {
   CHASE: {
-    SAPPHIRE: {
-      PREFERRED: getRandom([
-        "https://www.referyourchasecard.com/6j/QN3N8HQ4KX",
-      ]),
-    },
+    SAPPHIRE: getRandom(["https://www.referyourchasecard.com/6j/QN3N8HQ4KX"]),
     INKS: getRandom([
-      // "https://www.referyourchasecard.com/21o/98ZI1A53EK",
-      // "https://www.referyourchasecard.com/21o/1QKLYDOXDQ",
+      "https://www.referyourchasecard.com/21o/98ZI1A53EK",
+      "https://www.referyourchasecard.com/21o/1QKLYDOXDQ",
       "https://www.referyourchasecard.com/21o/YURXNKIY4Y",
     ]),
     FREEDOM: getRandom(["https://www.referyourchasecard.com/18o/9KKC4IK8LP"]),
     UNITED: getRandom(["https://www.referyourchasecard.com/215N/D2KTYJ4FTP"]),
     SOUTHWEST: getRandom([
-      "https://www.referyourchasecard.com/226N/0MJIAMX5UZ",
+      "https://www.referyourchasecard.com/226f/0MJIAMX5UZ",
     ]),
   },
   AMERICAN_EXPRESS: {
@@ -25,6 +21,6 @@ export const REFERRALS = {
     BUSINESS_GREEN:
       "https://americanexpress.com/en-us/referral/ANDENaewip?xl=cp19",
     DELTA_BUSINESS_PLATINUM:
-      "https://americanexpress.com/en-us/referral/ANDENaYGO3?xl=cp19",
+      "https://americanexpress.com/en-us/referral/delta-skymiles-platinum?ref=ANDENaLLR3&xl=cp19",
   },
 };
