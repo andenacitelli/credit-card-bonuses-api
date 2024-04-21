bc955e548c2776ae678202eb8d95947f5b11325c
Anden Acitelli <a.acitelli@gmail.com>
2024-04-21 12:08:26 -0500

    -jest +vitest +type=module

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,16 +1,17 @@
 import supertest from "supertest";
+import { describe, expect, test } from "vitest";
 import { app } from "./app";
 
 describe("Express app", () => {
   describe("/json", () => {
-    it("Returns array of nonzero length", async () => {
+    test("Returns array of nonzero length", async () => {
       const response = await supertest(app).get("/json");
       expect(response.statusCode).toEqual(200);
       expect(response.body.length).toBeGreaterThan(0);
     });
   });
   describe("/csv", () => {
-    it("Returns string of nonzero length", async () => {
+    test("Returns string of nonzero length", async () => {
       const response = await supertest(app).get("/csv");
       expect(response.statusCode).toEqual(200);
       expect(response.text.length).toBeGreaterThan(0);
