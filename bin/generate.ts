5eea69175bdfc3d8f10bc2a3688e9cd9564e9b8e
Anden Acitelli <a.acitelli@gmail.com>
2024-07-03 08:13:23 -0500

    general cleanup

--- bin/generate.ts
+++ bin/generate.ts
@@ -4,12 +4,9 @@ import {
   getDataInJsonFormat,
   getDataInYamlFormat,
 } from "@/conversion.js";
-import { mkdirp } from "mkdirp";
 import path from "node:path";
 
 const ROOT_FOLDER = "./exports";
-
-await mkdirp(ROOT_FOLDER);
 await Promise.all([
   fs.writeFile(path.join(ROOT_FOLDER, "data.json"), getDataInJsonFormat()),
   fs.writeFile(path.join(ROOT_FOLDER, "data.csv"), getDataInCsvFormat()),
