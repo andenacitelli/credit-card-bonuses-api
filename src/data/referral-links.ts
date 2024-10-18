ddd77c8556bc1874fdad5b2b979d54887655a734
Anden Acitelli <a.acitelli@gmail.com>
2024-10-18 14:38:14 -0500

    Bump Referral Links

--- src/data/referral-links.ts
+++ src/data/referral-links.ts
@@ -6,9 +6,10 @@ export const REFERRALS = {
   CHASE: {
     SAPPHIRE: getRandom(["https://www.referyourchasecard.com/19o/QN3N8HQ4KX"]),
     INKS: getRandom([
-      "https://www.referyourchasecard.com/21r/5XN33129Y3",
-      "https://www.referyourchasecard.com/21r/Z9CI8ZT4ON",
-      "https://www.referyourchasecard.com/21r/ONDY31NAZE",
+      "https://www.referyourchasecard.com/21r/URE6QWKIBT", // Premier
+      // "https://www.referyourchasecard.com/21r/5XN33129Y3",
+      // "https://www.referyourchasecard.com/21r/Z9CI8ZT4ON",
+      // "https://www.referyourchasecard.com/21r/ONDY31NAZE",
     ]),
     FREEDOM: getRandom(["https://www.referyourchasecard.com/18s/BXSOWPQ68F"]),
     UNITED: getRandom(["https://www.referyourchasecard.com/215A/OADUJ1AWMB"]),
