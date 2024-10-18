99e70d1663bab74df14befeff0dafe16da9d3786
Anden Acitelli <a.acitelli@gmail.com>
2024-10-18 14:39:56 -0500

    Bump Referral Links

--- src/data/referral-links.ts
+++ src/data/referral-links.ts
@@ -4,19 +4,19 @@ const getRandom = (array: string[]): string => {
 
 export const REFERRALS = {
   CHASE: {
-    SAPPHIRE: getRandom(["https://www.referyourchasecard.com/19o/QN3N8HQ4KX"]),
+    SAPPHIRE: getRandom(["https://www.referyourchasecard.com/19P/9WT5WQBFKH"]),
     INKS: getRandom([
       "https://www.referyourchasecard.com/21r/URE6QWKIBT", // Premier
       // "https://www.referyourchasecard.com/21r/5XN33129Y3",
       // "https://www.referyourchasecard.com/21r/Z9CI8ZT4ON",
       // "https://www.referyourchasecard.com/21r/ONDY31NAZE",
     ]),
-    FREEDOM: getRandom(["https://www.referyourchasecard.com/18s/BXSOWPQ68F"]),
+    FREEDOM: getRandom(["https://www.referyourchasecard.com/18C/BDWS964YDG"]),
     UNITED: getRandom(["https://www.referyourchasecard.com/215A/OADUJ1AWMB"]),
     SOUTHWEST: getRandom([
-      "https://www.referyourchasecard.com/226p/C2WKH1NBGL",
+      "https://www.referyourchasecard.com/226o/0MJIAMX5UZ",
     ]),
-    MARRIOTT: getRandom(["https://www.referyourchasecard.com/252j/1FMGX9HG9J"]),
+    MARRIOTT: getRandom(["https://www.referyourchasecard.com/252s/EE1YBTRY3X"]),
   },
   AMERICAN_EXPRESS: {
     GOLD: "https://americanexpress.com/en-us/referral/ANDENa0UQA",
