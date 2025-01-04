87c1c9424ad18e6496683bdc8fa9e6908f30c966
Anden Acitelli <a.acitelli@gmail.com>
2025-01-04 10:13:46 -0600

    Updates: Alaska Airlines tweak, referral tweaks

--- src/data/cards/bankofamerica.ts
+++ src/data/cards/bankofamerica.ts
@@ -53,11 +53,12 @@ export const BANK_OF_AMERICA_CARDS: z.input<typeof schemas.CreditCard>[] = [
     ],
     offers: [
       {
-        amount: [{ amount: 75_000 }],
+        amount: [{ amount: 70_000 }],
         spend: 3000,
         days: 90,
         credits: [],
         url: "http://alaska75k.com/",
+        details: "Extra 5k for naming an Alaska employee's name and ID.",
       },
       {
         amount: [{ amount: 60_000 }],
--- src/data/referral-links.ts
+++ src/data/referral-links.ts
@@ -6,9 +6,9 @@ export const REFERRALS = {
   CHASE: {
     SAPPHIRE: getRandom(["https://www.referyourchasecard.com/19Q/QN3N8HQ4KX"]),
     INKS: getRandom([
-      // "https://www.referyourchasecard.com/21s/63VP5J3DX1",
-      // "https://www.referyourchasecard.com/21s/9BZH8BJC1Z",
-      // "https://www.referyourchasecard.com/21s/B4FA0PBHTK",
+      "https://www.referyourchasecard.com/21s/63VP5J3DX1",
+      "https://www.referyourchasecard.com/21s/9BZH8BJC1Z",
+      "https://www.referyourchasecard.com/21s/B4FA0PBHTK",
       "https://www.referyourchasecard.com/21s/6I9J2FNU9T",
     ]),
     FREEDOM: getRandom(["https://www.referyourchasecard.com/18N/UXXI57YWJ7"]),
