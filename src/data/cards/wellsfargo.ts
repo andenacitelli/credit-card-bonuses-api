68ca2da3a53fb19a2afd362b526fbf572b691f62
Anden Acitelli <a.acitelli@gmail.com>
2025-04-15 08:41:20 -0500

    Choice Privileges 60k

--- src/data/cards/wellsfargo.ts
+++ src/data/cards/wellsfargo.ts
@@ -145,7 +145,7 @@ export const WELLS_FARGO_CARDS: Array<CreditCardInput> = [
     credits: [],
     offers: [
       {
-        amount: [{ amount: 40_000 }],
+        amount: [{ amount: 60_000 }],
         spend: 1000,
         days: 90,
         credits: [],
