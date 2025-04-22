8766b6f34b8e149019f7eb3f7a35151b80b31bd6
Anden Acitelli <a.acitelli@gmail.com>
2025-04-22 17:01:59 -0500

    Updates: Referrals

--- src/data/referral-links.ts
+++ src/data/referral-links.ts
@@ -4,23 +4,23 @@ const getRandom = (array: string[]): string => {
 
 export const REFERRALS = {
   CHASE: {
-    SAPPHIRE: getRandom(["https://www.referyourchasecard.com/19Q/QN3N8HQ4KX"]),
+    SAPPHIRE: getRandom(["https://www.referyourchasecard.com/19r/EKKGSTEZXG"]),
     INKS: getRandom([
-      "https://www.referyourchasecard.com/21s/63VP5J3DX1",
       "https://www.referyourchasecard.com/21s/9BZH8BJC1Z",
-      "https://www.referyourchasecard.com/21s/B4FA0PBHTK",
       "https://www.referyourchasecard.com/21s/6I9J2FNU9T",
+      "https://www.referyourchasecard.com/21s/63VP5J3DX1",
+      "https://www.referyourchasecard.com/21s/B4FA0PBHTK",
     ]),
-    FREEDOM: getRandom(["https://www.referyourchasecard.com/18J/BDWS964YDG"]),
-    UNITED: getRandom(["https://www.referyourchasecard.com/19q/AX64UADB74"]),
+    FREEDOM: getRandom(["https://www.referyourchasecard.com/18m/Y02RUGKSU9"]),
+    UNITED: getRandom(["https://www.referyourchasecard.com/215R/C3NRCBIUPP"]),
     SOUTHWEST: getRandom([
       "https://www.referyourchasecard.com/226m/EK4J5RUTDJ",
     ]),
-    MARRIOTT: getRandom(["https://www.referyourchasecard.com/252j/PSOE5X37UU"]),
+    MARRIOTT: getRandom(["https://www.referyourchasecard.com/252a/YV4FE606LH"]),
   },
   AMERICAN_EXPRESS: {
     BLUE_BUSINESS_PLUS: getRandom([
-      "https://americanexpress.com/en-us/referral/bluebusinessplus-credit-card?ref=ANDENaZ8L5&xl=cp10lg",
+      "https://americanexpress.com/en-us/referral/bluebusinessplus-credit-card?ref=ANDENaAniR&XLINK=MYCP",
     ]),
     // I don't seem to be able to refer people to my Gold for some reason
   },
